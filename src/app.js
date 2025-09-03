86aadcaee4d1b4599a90f2bd07aad8db323f0e36
danish-1999 <danishalam861@gmail.com>
2025-09-03 16:11:55 +0530

    Playing with query params

--- src/app.js
+++ src/app.js
@@ -3,24 +3,22 @@ const express = require("express");
 const app = express();
 
 
-
-
-
-app.get("/user",(req, res) => {
+app.get("/user/:userId/:Name/:Password",(req, res) => {
+  console.log(req.params)
   res.send({"firstName" : "Danish", "lastName" : "Alam"});
 });
 
-app.post("/user",(req, res) => {
-  res.send("Data is saved to the server.");
-});
+// app.post("/user",(req, res) => {
+//   res.send("Data is saved to the server.");
+// });
 
-app.delete("/user",(req, res) => {
-  res.send("Profile deleted successfully");
-});
+// app.delete("/user",(req, res) => {
+//   res.send("Profile deleted successfully");
+// });
 
-app.patch("/user",(req, res) => {
-  res.send("Profile updated successfully");
-});
+// app.patch("/user",(req, res) => {
+//   res.send("Profile updated successfully");
+// });
 
 
 app.listen(8888, () =>{
