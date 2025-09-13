4cae2de1ee6196433d8f01a8754d493492e7935f
danish-1999 <danishalam861@gmail.com>
2025-09-13 16:39:31 +0530

    Schema validations

--- src/app.js
+++ src/app.js
@@ -10,13 +10,96 @@ app.post("/signup", async(req, res) => {
   // Creating user account
   const user = new User(req.body);
   try {
-    
-  user.save();
-  res.send("User added succesfully...");
+    user.save().then(() => {
+      res.send("User added succesfully...");
+    }).catch(err =>{
+      res.status(404).send("User didn't connect " + err.message);
+    });
   } catch (error) {
-    res.status(400).send("User didn't connect");
+    res.status(400).send("User didn't connect" + error.message);
   }
 })
+
+// Get user account using his/her emailId
+
+app.get("/userByEmail", async(req, res) => {
+  try{
+    const user = await User.findOne({emailId : req.body.emailId});
+    if(!user)
+      res.status(404).send("User not found");
+    else 
+      res.send(user);
+  }catch(err){
+    res.status(400).send("something went wrong");
+  }
+})
+
+// Get all users account
+app.get("/allUser", async(req, res) => {
+  try{
+    const users = await User.find();
+    if(!users)
+        res.status(404).send("No user is there!");
+    else
+      res.send(users);
+  }catch(err){
+    res.status(400).send("something went wrong");
+  }
+})
+
+// Get user acount using their Ids
+app.get("/userById", async(req, res) => {
+  try {
+    const user = await User.findById(req.body._id);
+    if (!user) 
+      res.status(404).send("User not found");
+    else 
+      res.send(user);
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+})
+
+// Delete a user account
+app.delete("/deleteUser", async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.body.userId);
+    res.send("User deleted succefully!!");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+})
+
+// Update details of user
+app.patch("/updataUser", async(req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.body.userId, req.body);
+    if(!user)
+      res.status(404).send("User not found");
+    else {
+      console.log(user);
+      res.send("user update succefully!!");
+    }
+  } catch (error) {
+    res.status(400).send("User id not updated" + error.message);
+  }
+})
+
+// update using emnailId
+app.patch("/updateByEmail", async(req, res) => {
+  try {
+    const user = await User.findOneAndUpdate({emailId : req.body.emailId}, req.body);
+    if(!user)
+      res.status(404).send("User not found");
+    else {
+      console.log(user);
+      res.send("User updated succefully!!")
+    }
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+})
+
 
 connectDB()
  .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,49 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
- firstName: { type: String },
- lastName: {type: String, },
- email: {type :  String, },
- password: {type :  String, },
- age: {type : Number, },
- gender: {type :  String, },
- interested: {type :  String, },
-});
+  firstName: { 
+    type: String,
+    required: true,
+    minLength: 4,
+    maxLength: 15,
+    trim: true
+  },
+  lastName: {
+    type: String, 
+    minLength: 3,
+    maxLength: 10,
+    trim: true
+  },
+  emailId: {
+    type :  String, 
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true
+  },
+  password: {
+    type :  String, 
+    required: true
+  },
+  age: {
+    type: Number, 
+    min: 18 
+  },
+  gender: {
+    type :  String, 
+    validate(value){
+      if(!["male","female","others"].includes(value))
+        res.status(400).send("Gender data is not correct"); 
+    }
+  },
+  about: {
+    type: String,
+    default: "Give some descriptin about yuorself."
+  },
+  skills: {type :  [String], },
+},
+{ 
+  timestamps: true 
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
