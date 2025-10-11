fa23776c60ff77a2cd8c51ad1af131d934cd5364
danish-1999 <danishalam861@gmail.com>
2025-10-11 12:20:41 +0530

    build: logout feature

--- src/mildlewares/auth.js
+++ src/mildlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try{
     const { token } = req.cookies;
     if (!token) {
-      throw new Error("Cookie expired please login agian");
+      return res.status(401).send("Please login");
     }
     const decodedMessage = jwt.verify(token, "Tinder@clone_2025");
     const {_id} = decodedMessage;
